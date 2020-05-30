e50fd0241f819b59c93536b16bfa6aa4f34c78c2
d0n <d0n@janeiskla.de>
2020-05-30 11:28:46 +0200

    added setup.py for releaseing

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pavlovadm',
+      version='0.1',
+      description='utility to manage pavlov\'s rcon like admin interface',
+      author='d0n',
+      author_email='d0n@janeiskla.de',
+      url='https://www.python.org/d0n/pavlovadm/',
+      packages=[],
+     )
